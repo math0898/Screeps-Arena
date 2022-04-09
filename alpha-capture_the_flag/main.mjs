0bec80932c38a06e349eb5a30b6ec8e255a103d8
math0898 <25396616+math0898@users.noreply.github.com>
2022-04-09 01:01:20 -0500

    Polish Version 09
    - Polished and fixed up bugs present in version 09.

--- alpha-capture_the_flag/main.mjs
+++ alpha-capture_the_flag/main.mjs
@@ -1,20 +1,22 @@
 import { getObjectsByPrototype, findInRange } from '/game/utils';
 import { Creep, Flag, StructureTower } from '/game/prototypes';
-import { ATTACK, RANGED_ATTACK, HEAL } from '/game/constants';
+import { ATTACK, RANGED_ATTACK, HEAL, MOVE } from '/game/constants';
 import { } from '/arena';
 import { getTicks } from '/game';
 import { Ranger } from "./Ranger.mjs";
 import { Healer } from './Healer.mjs';
 import { Melee } from './Melee.mjs';
 
-function init () {
+var creeps = new Array();
+var mode = 'defend';
 
-    const enemyFlag = getObjectsByPrototype(Flag).find(f => !f.my);
-    const myFlag = getObjectsByPrototype(Flag).find(f => f.my);
+function init () {
 
     var melees = getObjectsByPrototype(Creep).filter(c => c.body.some(body => body.type == ATTACK) && c.my);
     var ranged = getObjectsByPrototype(Creep).filter(c => c.body.some(body => body.type == RANGED_ATTACK) && c.my);
     var healers = getObjectsByPrototype(Creep).filter(c => c.body.some(body => body.type == HEAL) && c.my);
+    const enemyFlag = getObjectsByPrototype(Flag).find(f => !f.my);
+    const myFlag = getObjectsByPrototype(Flag).find(f => f.my);
 
     for (var r of ranged) creeps.push(new Ranger(r, myFlag, enemyFlag));
     for (var h of healers) creeps.push(new Healer(h, myFlag, enemyFlag));
@@ -35,37 +37,45 @@ function towerTime (t) {
     }
 }
 
-var creeps = new Array();
-var mode = 'defend';
-
-export function loop () {
+export function loop () { // todo anti-turtle strategy
 
     const tick = getTicks();
 
     var enemies = getObjectsByPrototype(Creep).filter(c => !c.my);
     var allies = getObjectsByPrototype(Creep).filter(c => c.my);
     var towers = getObjectsByPrototype(StructureTower).filter(t => t.my);
+    const enemyFlag = getObjectsByPrototype(Flag).find(f => !f.my);
+    const myFlag = getObjectsByPrototype(Flag).find(f => f.my);
 
     if (tick == 1) init();
     else if (tick == 60) {
+        mode = 'defend';
         console.log('Switching to defense.');
         for (var c of creeps) c.setMode('defend');
-    } else if (tick == 1500) {
+    } else if (tick == 1500 || enemies.length == 0 && mode != 'attack') {
+        mode = 'attack';
         console.log('Switching to attack.');
         for (var c of creeps) c.setMode('attack');
     }
 
-    if (melees.length + ranged.length < 1 && mode != 'rush') {
+    if (getObjectsByPrototype(Creep).filter(c => c.my && c.body.some(body => body.type != MOVE && body.type != HEAL)) < 1 && mode != 'rush') {
         mode = 'rush';
         console.log('Switching to rush.');
         for (var c of creeps) c.setMode('rush');
     }
 
     if (tick < 60) { // TODO: create a defensive formation around flag.
+
+        const melees = getObjectsByPrototype(Creep).filter(c => c.body.some(body => body.type == ATTACK) && c.my);
+        const ranged = getObjectsByPrototype(Creep).filter(c => c.body.some(body => body.type == RANGED_ATTACK) && c.my);
+        const healers = getObjectsByPrototype(Creep).filter(c => c.body.some(body => body.type == HEAL) && c.my);
+
         for (var h of healers) h.moveTo(myFlag);
         for (var r of ranged) r.moveTo(myFlag);
         for (var m of melees) m.moveTo(myFlag);
     }
+
+    if (mode == 'defend') enemies = findInRange(myFlag, enemies, 10);
 
     if (tick >= 60) {
         for (var c of creeps) c.runLogic(enemies, allies);
