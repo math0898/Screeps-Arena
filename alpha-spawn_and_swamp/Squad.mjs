804f86b9df820bc26d89e799e0fc6cc6055943df
math0898 <25396616+math0898@users.noreply.github.com>
2023-09-08 21:48:00 -0500

    Added Squads
    - Squads seem to have inconsistent behavior.

--- alpha-spawn_and_swamp/Squad.mjs
+++ alpha-spawn_and_swamp/Squad.mjs
@@ -1,4 +1,4 @@
-import { Creep } from "game/prototypes";
+import { Creep, StructureSpawn } from "game/prototypes";
 import { getObjectsByPrototype } from "game/utils";
 
 /**
@@ -66,9 +66,11 @@ Squad.prototype.moveTo = function (pos, checkMerged = true) {
 
 Squad.prototype.logic = function () {
     const enemies = getObjectsByPrototype(Creep).filter((c) => !c.my);
+    const structures = getObjectsByPrototype(StructureSpawn).filter(s => !s.my);
     const inRangeEnemies = this.rangers[0].findInRange(enemies, 4);
     if (inRangeEnemies.length > 3) for (let c in this.rangers) this.rangers[c].rangedMassAttack(); // TODO: Little more nuance needed
     else if (inRangeEnemies.length > 0) for (let c in this.rangers) this.rangers[c].rangedAttack(inRangeEnemies[0]); // TODO: Lowest Health or healers
+    else for (let c in this.rangers) this.rangers[c].rangedAttack(structures[0]);
     var lowest = this.rangers[0];
     if (this.rangers[1].hits < lowest.hits) lowest = this.rangers[1];
     for (let i in this.healers) if (this.healers[i].hits < lowest.hits) lowest = this.healers[i];
--- alpha-spawn_and_swamp/main.mjs
+++ alpha-spawn_and_swamp/main.mjs
@@ -1,7 +1,9 @@
 // @ts-nocheck
-import { getObjectsByPrototype } from 'game/utils';
+import { getObjectsByPrototype, getTerrainAt } from 'game/utils';
+import { CostMatrix } from "game/path-finder";
 import { StructureSpawn, Creep, StructureContainer } from 'game/prototypes';
-import { ATTACK, MOVE, CARRY, ERR_NOT_IN_RANGE, RANGED_ATTACK, RESOURCE_ENERGY, HEAL } from 'game/constants';
+import { ATTACK, MOVE, CARRY, ERR_NOT_IN_RANGE, RANGED_ATTACK, RESOURCE_ENERGY, HEAL, TERRAIN_PLAIN, TERRAIN_SWAMP, TERRAIN_WALL } from 'game/constants';
+import { Squad } from './Squad.mjs';
 
 var fillers = new Array();
 var squads = new Array();
@@ -9,6 +11,24 @@ var awaitingSquad = new Array();
 const mySpawn = getObjectsByPrototype(StructureSpawn).find(s => s.my);
 const enemySpawn = getObjectsByPrototype(StructureSpawn).find(s => !s.my);
 var added = true;
+var squadMatrix = new CostMatrix();
+
+var findSquadMatrix = function () {
+    let matrix = new CostMatrix();
+    for (let y = 0; y < 100; y++) {
+        for (let x = 0; x < 100; x++) {
+            let tile = getTerrainAt({x: x, y: y});
+            let weight =
+                tile === TERRAIN_WALL  ? 255 : // wall  => unwalkable
+                tile === TERRAIN_SWAMP ?   5 : // swamp => weight:  5
+                                            1 ; // plain => weight:  1
+        matrix.set(x, y, weight);
+        }
+    }
+    for (let y = 1; y < 100; y++) for (let x = 1; x < 100; x++) squadMatrix.set(x, y, Math.max(matrix.get(x - 1, y - 1), matrix.get(x, y - 1), matrix.get(x - 1, y), matrix.get(x, y)));
+};
+
+findSquadMatrix();
 
 export function loop() {
     if (!added) {
@@ -27,7 +47,10 @@ export function loop() {
         added = false;
     }
 
-    if (awaitingSquad.length == 4); // TODO: Create Squad
+    if (awaitingSquad.length == 4) {
+        squads.push(new Squad(awaitingSquad.pop(), awaitingSquad.pop(), awaitingSquad.pop(), awaitingSquad.pop()));
+        squads[squads.length - 1].setCostMatrix(squadMatrix);
+    }
 
     // Filler Logic
     let containers = getObjectsByPrototype(StructureContainer).filter(s => s.store.getUsedCapacity() > 0);
@@ -37,6 +60,13 @@ export function loop() {
             let container = filler.findClosestByPath(containers);
             if (filler.withdraw(container, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) filler.moveTo(container);
         } else if (filler.transfer(mySpawn, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) filler.moveTo(mySpawn);
+    }
+
+    // Squad Logic
+    for (let s in squads) {
+        let squad = squads[s];
+        squad.moveTo(enemySpawn);
+        squad.logic();
     }
 
     // let creeps = getObjectsByPrototype(Creep).filter(c => c.my);
